18a7e61c3a3fbc7133743c709d0b4205691e245c
1E10 <sample@bths.edu>
2022-09-28 14:58:18 -0400

    fixed the error

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class TipCalculator {
     public static void main(String[] args) {
 
+
         Scanner scan = new Scanner(System.in);
         DecimalFormat formatter = new DecimalFormat("#.##");
 
@@ -20,6 +21,7 @@ public class TipCalculator {
         //variables//
         double totalBill = 0;
         double updatePrice = 0;
+        
 
         while (updatePrice >=0) {
             System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (Type -1 to end): ");
@@ -27,9 +29,35 @@ public class TipCalculator {
             updatePrice = scan.nextDouble();
             }
 
-        //receipt code//
-        System.out.println("-------------------------");
+        //total bill and tip percentage//
+        System.out.println("------------------------------");
         System.out.println("Total bill before tip: " + totalBill);
+        System.out.println("Tip percentage: " + tipPercent);
+
+        //tip variable
+        double totalTip = totalBill * (double)tipPercent / 100;
+        String formattedTip = formatter.format(totalTip);
+        System.out.println("Total Tip: " + formattedTip);
+
+        //total bill with tip
+        totalBill += totalTip;
+        String formattedBill = formatter.format(totalBill);
+        System.out.println("Total Bill with tip: " + formattedBill);
+
+        //total bill before tip divided amongst totalPeople
+        double costPer = (totalBill - totalTip) / totalPeople;
+        String formattedCost = formatter.format(costPer);
+        System.out.println("Per person cost before tip: " + formattedCost);
+
+        //tip per person
+        double tipPer = totalTip / totalPeople;
+        String formattedTipPer = formatter.format(tipPer);
+        System.out.println("Tip per person: " + formattedTipPer);
+
+        //total cost per person
+        double totalCostPer = totalBill / totalPeople;
+        String formattedTotalPer = formatter.format(totalCostPer);
+        System.out.println("Total cost per person: " + formattedTotalPer);
 
         scan.close();
 
