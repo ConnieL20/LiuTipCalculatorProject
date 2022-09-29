a46f7fbcd7d9dcd1671d7287185e6a30ad63dfd8
student <sample@bths.edu>
2022-09-29 10:53:23 -0400

    9/29/22

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 public class TipCalculator {
     public static void main(String[] args) {
 
-
         Scanner scan = new Scanner(System.in);
         DecimalFormat formatter = new DecimalFormat("#.##");
 
-        System.out.println("Welcome to the Tip Calculator!");
-        System.out.println("------------------------------");
+        //introduction
+        System.out.println("----------------------------------");
+        System.out.println("| Welcome to the Tip Calculator! |");
+        System.out.println("----------------------------------");
         System.out.print("How many people are in your group? ");
         int totalPeople = scan.nextInt();
         scan.nextLine();
@@ -18,49 +19,52 @@ public class TipCalculator {
         int tipPercent = scan.nextInt();
         scan.nextLine();
 
-        //variables//
+        //variables for the while loop//
         double totalBill = 0;
         double updatePrice = 0;
         
-
+        //iterates until the user types in -1
         while (updatePrice >=0) {
             System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (Type -1 to end): ");
             totalBill += updatePrice;
             updatePrice = scan.nextDouble();
             }
 
-        //total bill and tip percentage//
-        System.out.println("------------------------------");
-        System.out.println("Total bill before tip: " + totalBill);
-        System.out.println("Tip percentage: " + tipPercent);
+        //prints total bill and tip percentage//
+        System.out.println("---------------------------------------");
+        String formattedBillBefore = formatter.format(totalBill);
+        System.out.println("Total bill before tip: " + "$" + formattedBillBefore);
+        System.out.println("Tip percentage: " + tipPercent + "%");
 
-        //tip variable
+        //prints tip variable
         double totalTip = totalBill * (double)tipPercent / 100;
         String formattedTip = formatter.format(totalTip);
-        System.out.println("Total Tip: " + formattedTip);
+        System.out.println("Total Tip: " + "$" + formattedTip);
 
-        //total bill with tip
+        //prints total bill with tip
         totalBill += totalTip;
         String formattedBill = formatter.format(totalBill);
-        System.out.println("Total Bill with tip: " + formattedBill);
+        System.out.println("Total Bill with tip: " + "$" + formattedBill);
 
-        //total bill before tip divided amongst totalPeople
+        //prints total bill before tip divided amongst totalPeople
         double costPer = (totalBill - totalTip) / totalPeople;
         String formattedCost = formatter.format(costPer);
-        System.out.println("Per person cost before tip: " + formattedCost);
+        System.out.println("Per person cost before tip: " + "$" + formattedCost);
 
-        //tip per person
+        //prints tip per person
         double tipPer = totalTip / totalPeople;
         String formattedTipPer = formatter.format(tipPer);
-        System.out.println("Tip per person: " + formattedTipPer);
+        System.out.println("Tip per person: " + "$" + formattedTipPer);
 
-        //total cost per person
+        //prints total cost per person
         double totalCostPer = totalBill / totalPeople;
         String formattedTotalPer = formatter.format(totalCostPer);
-        System.out.println("Total cost per person: " + formattedTotalPer);
+        System.out.println("Total cost per person: " + "$" + formattedTotalPer);
+        System.out.println("---------------------------------------");
 
+        //prints farewell
+        System.out.println("Thank you for using the Tip Calculator!");
         scan.close();
-
 
         }
     }
